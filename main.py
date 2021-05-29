781d7622c57bcc81ad384f139a0bb69e8eeb22f1
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-05-29 02:08:09 -0300

    implementado melhoria no código conforme o andamento da aula

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 
-url_base = url[0:19]
-print(url_base)
+# Seoara a base e os parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+print(f'\nURL base: {url_base}')
+url_paremetros = url[indice_interrogacao+1:]
+print(f'URL parâmetros: {url_paremetros}')
 
-url_paremetros = url[20:36]
-print(url_paremetros)
+# Busca o valor de um parâmetro
+parametro_busca = 'moedaDestino'
+indice_parametro = url_paremetros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_paremetros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_paremetros[indice_valor:]
+else:
+    valor = url_paremetros[indice_valor:indice_e_comercial]
+print(valor)
